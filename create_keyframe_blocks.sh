16b8c9f1e5421f2580a057f4ce2bf7187433a2c8
Adrian Schuepbach <adrian.schuepbach@gribex.net>
2019-09-19 00:22:13 +0200

    Ensure that the moov tag is in each segment and after the keyframe.

--- create_keyframe_blocks.sh
+++ create_keyframe_blocks.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
 
 #ffmpeg -i example1_134.mp4 -acodec copy -f segment -vcodec copy -reset_timestamps 1 -map 0 OUTPUT%d.mp4
-ffmpeg -i $1 -acodec copy -f segment -vcodec copy -reset_timestamps 1 -map 0 %08d.mp4
+#ffmpeg -i $1 -f segment -acodec copy  -vcodec copy -movflags empty_moov -movflags frag_keyframe -reset_timestamps 1 -map 0 %08d.mp4
+#ffmpeg -i $1 -f segment -acodec copy  -vcodec copy -movflags empty_moov  -reset_timestamps 1 -map 0 %08d.mp4
+
+#ffmpeg -i $1 -acodec copy  -vcodec copy -movflags frag_keyframe $1.neu.mp4
+#ffmpeg -i $1.neu.mp4 -f segment -acodec copy  -vcodec copy -movflags frag_keyframe -reset_timestamps 1 -map 0 %08d.mp4
+
+
+ffmpeg -i $1 -f segment -acodec copy  -vcodec copy -movflags frag_keyframe -reset_timestamps 1 -map 0 %08d.mp4
+
